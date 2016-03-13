6c9b6a6ad66989c694ba9e7953f74956febfe740
Miranda Michalski <mbrockhaus@gmail.com>
2016-03-13 11:02:44 -0400

    initial commit

---
+++ fileB.js
@@ -0,0 +1,102 @@
+lrwxrwxrwx 1 myTraining 197121      35 Oct 12  2014 ../../Application Data -> /c/Users/myTraining/AppData/Roaming/
+lrwxrwxrwx 1 myTraining 197121      63 Oct 12  2014 ../../Cookies -> /c/Users/myTraining/AppData/Local/Microsoft/Windows/INetCookies/
+lrwxrwxrwx 1 myTraining 197121      33 Oct 12  2014 ../../Local Settings -> /c/Users/myTraining/AppData/Local/
+lrwxrwxrwx 1 myTraining 197121      29 Oct 12  2014 ../../My Documents -> /c/Users/myTraining/Documents/
+lrwxrwxrwx 1 myTraining 197121      71 Oct 12  2014 ../../NetHood -> /c/Users/myTraining/AppData/Roaming/Microsoft/Windows/Network Shortcuts/
+-rw-r--r-- 1 myTraining 197121  786432 Mar 12 14:58 ../../NTUSER.DAT
+-rw-r--r-- 1 myTraining 197121 1159168 Oct 12  2014 ../../ntuser.dat.LOG1
+-rw-r--r-- 1 myTraining 197121  327680 Oct 12  2014 ../../ntuser.dat.LOG2
+-rw-r--r-- 1 myTraining 197121   65536 Mar 12 14:58 ../../NTUSER.DAT{d4156549-ae6c-11e3-80bb-90b11c2537ec}.TM.blf
+-rw-r--r-- 1 myTraining 197121  524288 Mar 12 14:58 ../../NTUSER.DAT{d4156549-ae6c-11e3-80bb-90b11c2537ec}.TMContainer00000000000000000001.regtrans-ms
+-rw-r--r-- 1 myTraining 197121  524288 Mar 12 14:58 ../../NTUSER.DAT{d4156549-ae6c-11e3-80bb-90b11c2537ec}.TMContainer00000000000000000002.regtrans-ms
+-rw-r--r-- 1 myTraining 197121      20 Oct 12  2014 ../../ntuser.ini
+lrwxrwxrwx 1 myTraining 197121      71 Oct 12  2014 ../../PrintHood -> /c/Users/myTraining/AppData/Roaming/Microsoft/Windows/Printer Shortcuts/
+lrwxrwxrwx 1 myTraining 197121      60 Oct 12  2014 ../../Recent -> /c/Users/myTraining/AppData/Roaming/Microsoft/Windows/Recent/
+lrwxrwxrwx 1 myTraining 197121      60 Oct 12  2014 ../../SendTo -> /c/Users/myTraining/AppData/Roaming/Microsoft/Windows/SendTo/
+lrwxrwxrwx 1 myTraining 197121      64 Oct 12  2014 ../../Start Menu -> /c/Users/myTraining/AppData/Roaming/Microsoft/Windows/Start Menu/
+lrwxrwxrwx 1 myTraining 197121      63 Oct 12  2014 ../../Templates -> /c/Users/myTraining/AppData/Roaming/Microsoft/Windows/Templates/
+
+../../AppData:
+total 4
+drwxr-xr-x 1 myTraining 197121 0 Mar 13 09:58 Local/
+drwxr-xr-x 1 myTraining 197121 0 Mar 12 08:16 LocalLow/
+drwxr-xr-x 1 myTraining 197121 0 Mar 12 08:16 Roaming/
+
+../../Contacts:
+total 1
+-rw-r--r-- 1 myTraining 197121 412 Oct 12  2014 desktop.ini
+
+../../Desktop:
+total 1
+-rw-r--r-- 1 myTraining 197121 282 Oct 12  2014 desktop.ini
+
+../../Documents:
+total 1
+-rw-r--r-- 1 myTraining 197121 402 Oct 12  2014 desktop.ini
+lrwxrwxrwx 1 myTraining 197121  25 Oct 12  2014 My Music -> /c/Users/myTraining/Music/
+lrwxrwxrwx 1 myTraining 197121  28 Oct 12  2014 My Pictures -> /c/Users/myTraining/Pictures/
+lrwxrwxrwx 1 myTraining 197121  26 Oct 12  2014 My Videos -> /c/Users/myTraining/Videos/
+
+../../Downloads:
+total 37405
+-rw-r--r-- 1 myTraining 197121      282 Oct 12  2014 desktop.ini
+-rwxr-xr-x 1 myTraining 197121 30749808 Mar 12 08:24 Git-2.7.2-32-bit.exe*
+-rwxr-xr-x 1 myTraining 197121  7548888 Mar 12 13:07 Sublime Text Build 3103 Setup.exe*
+
+../../exercise1:
+total 3
+-rw-r--r-- 1 myTraining 197121 18 Mar 12 14:10 andrew.txt
+-rw-r--r-- 1 myTraining 197121 35 Mar 12 14:09 bonnie.js
+-rw-r--r-- 1 myTraining 197121 53 Mar 12 14:10 daniel.js
+
+../../Favorites:
+total 6
+drwxr-xr-x 1 myTraining 197121   0 Oct 12  2014 ASUS E-Service/
+-rw-r--r-- 1 myTraining 197121 208 Oct 12  2014 Bing.url
+-rw-r--r-- 1 myTraining 197121 402 Oct 12  2014 desktop.ini
+drwxr-xr-x 1 myTraining 197121   0 Oct 12  2014 Links/
+
+../../gdi-git:
+total 16
+drwxr-xr-x 1 myTraining 197121 0 Mar 13 10:18 day2warmup/
+drwxr-xr-x 1 myTraining 197121 0 Mar 12 14:22 exercise1/
+drwxr-xr-x 1 myTraining 197121 0 Mar 13 10:27 exercise2/
+drwxr-xr-x 1 myTraining 197121 0 Mar 13 11:01 exercise3A/
+drwxr-xr-x 1 myTraining 197121 0 Mar 13 11:01 exercise3B/
+drwxr-xr-x 1 myTraining 197121 0 Mar 13 10:31 git-cmd-demos/
+
+../../IntelGraphicsProfiles:
+total 24
+-rw-r--r-- 1 myTraining 197121 8148 Oct 12  2014 Brighten Video.man.igpi
+-rw-r--r-- 1 myTraining 197121 8148 Oct 12  2014 Darken Video.man.igpi
+-rw-r--r-- 1 myTraining 197121 8148 Oct 12  2014 Enhance Video Colors.man.igpi
+
+../../Links:
+total 7
+-rw-r--r-- 1 myTraining 197121 580 Oct 12  2014 desktop.ini
+-rwxr-xr-x 1 myTraining 197121 452 Oct 12  2014 Desktop.lnk*
+-rwxr-xr-x 1 myTraining 197121 877 Oct 12  2014 Downloads.lnk*
+-rwxr-xr-x 1 myTraining 197121 363 Oct 12  2014 RecentPlaces.lnk*
+
+../../Music:
+total 1
+-rw-r--r-- 1 myTraining 197121 504 Oct 12  2014 desktop.ini
+
+../../Pictures:
+total 1
+-rw-r--r-- 1 myTraining 197121 504 Oct 12  2014 desktop.ini
+
+../../Saved Games:
+total 1
+-rw-r--r-- 1 myTraining 197121 282 Oct 12  2014 desktop.ini
+
+../../Searches:
+total 7
+-rw-r--r-- 1 myTraining 197121 524 Oct 12  2014 desktop.ini
+-r--r--r-- 1 myTraining 197121 248 Oct 12  2014 Everywhere.search-ms
+-r--r--r-- 1 myTraining 197121 248 Oct 12  2014 Indexed Locations.search-ms
+-rw-r--r-- 1 myTraining 197121 855 Oct 12  2014 winrt--{S-1-5-21-1189707859-1906995194-215127120-1001}-.searchconnector-ms
+
+../../Videos:
+total 1
+-rw-r--r-- 1 myTraining 197121 504 Oct 12  2014 desktop.ini
